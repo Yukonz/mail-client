ad68794f2a1f60f471bf72afabe006dd90575bc1
yukon <wiktor.g@live.com>
2017-06-02 20:13:48 +0300

    Remove copy folder

--- copy/application/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$route['news/create'] = 'news/create';
-$route['news/(:any)'] = 'news/view/$1';
-$route['news'] = 'news';
-$route['(:any)'] = 'home/view/$1';
-$route['default_controller'] = 'news';
